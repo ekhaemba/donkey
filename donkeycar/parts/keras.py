e529f84c5d5825b4b171ab919d640999e0959bb7
Elton Khaemba <ekhaemba@udel.edu>
2018-03-18 13:15:46 -0400

    Format printing

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -79,10 +79,10 @@ class KerasCategorical(KerasPilot):
     def run(self, img_arr):
         img_arr = img_arr.reshape((1,) + img_arr.shape)
         angle_binned, throttle = self.model.predict(img_arr)
-        print('throttle', throttle)
+        print('throttle: {}'.format(throttle))
         #angle_certainty = max(angle_binned[0])
         angle_unbinned = dk.utils.linear_unbin(angle_binned)
-        print('Angle unbinned: {}, Throttle: {}', angle_unbinned, throttle[0][0])
+        print('Angle unbinned: {}, Throttle: {}'.format(angle_unbinned, throttle[0][0]))
         return angle_unbinned, throttle[0][0]
 
 
