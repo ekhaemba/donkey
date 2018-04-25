642decfd2f268cd3c750adc184ab00f6cc637040
Elton Khaemba <ekhaemba@udel.edu>
2018-04-25 15:29:04 -0400

    Fuck spaces

--- donkeycar/parts/controller.py
+++ donkeycar/parts/controller.py
@@ -299,38 +299,38 @@ class JoystickController(object):
                     self.mode = 'user'
                 print('new mode:', self.mode)
 
-             if button == 'circle' and button_state == 1:
-                 '''
-                 toggle recording on/off
-                 '''
-                 if self.auto_record_on_throttle:
-                     print('auto record on throttle is enabled.')
-                 elif self.recording:
-                     self.recording = False
-                 else:
-                     self.recording = True
+            if button == 'circle' and button_state == 1:
+                '''
+                toggle recording on/off
+                '''
+                if self.auto_record_on_throttle:
+                    print('auto record on throttle is enabled.')
+                elif self.recording:
+                    self.recording = False
+                else:
+                    self.recording = True
 
             #     print('recording:', self.recording)
 
-             if button == 'triangle' and button_state == 1:
+            if button == 'triangle' and button_state == 1:
                  '''
                  increase max throttle setting
                  '''
-                 self.max_throttle = round(min(1.0, self.max_throttle + 0.01), 2)
-                 if self.constant_throttle:
-                     self.throttle = self.max_throttle
-                     self.on_throttle_changes()
+                self.max_throttle = round(min(1.0, self.max_throttle + 0.01), 2)
+                if self.constant_throttle:
+                    self.throttle = self.max_throttle
+                    self.on_throttle_changes()
 
             #     print('max_throttle:', self.max_throttle)
 
-             if button == 'cross' and button_state == 1:
+            if button == 'cross' and button_state == 1:
                  '''
                  decrease max throttle setting
                  '''
-                 self.max_throttle = round(max(0.0, self.max_throttle - 0.01), 2)
-                 if self.constant_throttle:
-                     self.throttle = self.max_throttle
-                     self.on_throttle_changes()
+                self.max_throttle = round(max(0.0, self.max_throttle - 0.01), 2)
+                if self.constant_throttle:
+                    self.throttle = self.max_throttle
+                    self.on_throttle_changes()
 
             #     print('max_throttle:', self.max_throttle)
 
